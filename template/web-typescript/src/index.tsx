e37f18d60718bbb7a890569e0305093476ac35af
luoluoqixi <luoqixi@luoqixi.com>
2024-02-15 01:26:02 +0800

    feat: update template

--- template/web-typescript/src/index.tsx
+++ template/web-typescript/src/index.tsx
@@ -1,5 +1,6 @@
 import { Refast } from '@refastdev/refast';
+import { pages } from '@refastdev/refast/routes';
 import { createRoot } from 'react-dom/client';
 
 const root = document.getElementById('root') as HTMLElement;
-createRoot(root).render(<Refast />);
+createRoot(root).render(<Refast routesOption={{ pages }} />);
